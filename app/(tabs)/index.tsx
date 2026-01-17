23512db7acbe47e857df5777e6e49ad8f1c7fb33
Emmanuel <emmsom506@gmail.com>
2026-01-17 17:09:26 +0000

    Add listed posts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,18 @@
 import {SafeAreaView} from "react-native-safe-area-context";
 import TabsTopNav from "@/components/layout/tabs-top-nav";
 import {HomePage} from "@/components/feature/home-page";
+import {MessageCircleMore} from "lucide-react-native";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView
-      edges={['top', 'right', 'left']}
-      style={{ flex: 1, paddingHorizontal: 16 }}
+      edges={['top']}
+      style={{ flex: 1}}
     >
-      <TabsTopNav id={"home-screen"}/>
+      <TabsTopNav
+        id={"home-screen"}
+        rightContent={<MessageCircleMore size={24}/>}
+      />
       <HomePage/>
     </SafeAreaView>
   );
